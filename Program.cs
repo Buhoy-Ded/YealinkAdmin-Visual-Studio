3be700de0735421e3de6a3fa9ed52d98f83113f4
BuhoyDed <zver2030zver@gmail.com>
2026-05-23 11:47:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.StaticFiles;
+using YealinkAdmin.Components;
+using YealinkAdmin.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Включаем StaticWebAssets для Production
+builder.WebHost.UseStaticWebAssets();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDataProtection();
+builder.Services.AddSingleton < SecureCredentialStorage > ();
+builder.Services.AddSingleton < YealinkApiClient > ();
+builder.Services.AddSingleton < PhoneStore > ();
+builder.Services.AddSingleton < YealinkScanner > ();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents < App > ()
+    .AddInteractiveServerRenderMode();
+
+var store = app.Services.GetRequiredService < PhoneStore > ();
+store.Load();
+
+app.Run();
+
+yv$5$w@K&574zN3$4h@L5q*2@^#G4Eb#
---
+++ Services/SecureCredentialStorage.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace YealinkAdmin.Services;
+
+public class SecureCredentialStorage(IDataProtectionProvider provider)
+{
+    private string? _encryptedUsername;
+    private string? _encryptedPassword;
+    private readonly IDataProtector _protector = provider.CreateProtector("Yealink");
+
+    public bool IsConfigured => _encryptedUsername != null;
+
+    public void Set(string username, string password)
+    {
+        _encryptedUsername = _protector.Protect(username);
+        _encryptedPassword = _protector.Protect(password);
+    }
+
+    public (string username, string password)? Get()
+    {
+        if (_encryptedUsername == null || _encryptedPassword == null) return null;
+        return (_protector.Unprotect(_encryptedUsername), _protector.Unprotect(_encryptedPassword));
+    }
+
+    public void Clear()
+    {
+        _encryptedUsername = null;
+        _encryptedPassword = null;
+    }
+}
